0ebcd52125a794987b4bc509bcd7e31873adea3c
Júlio César <juliocesarsouzacarvalho284@gmail.com>
2025-04-03 15:15:36 -0300

    Adding features on ContactAdmin class

--- contact/admin.py
+++ contact/admin.py
@@ -3,5 +3,11 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
-
+    list_display = 'first_name', 'last_name', 'phone', 'email',
+    ordering = '-id', 
+    # list_filter = 'created_date',
+    search_fields = 'id', 'first_name', 'last_name', 'email', 'phone',
+    list_per_page = 30
+    list_max_show_all = 100
+    list_editable = 'email',
+    list_display_links = 'first_name', 'last_name',
