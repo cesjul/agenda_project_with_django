9869d11b951ad6cf498c54f7095550cbb05a9dda
Júlio César <juliocesarsouzacarvalho284@gmail.com>
2025-04-03 14:23:18 -0300

    Creating model contact, with it migrations

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=250)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TimeField(blank=True)
