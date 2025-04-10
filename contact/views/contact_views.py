544f9683d14a39961ca848fe6d7c921c736ba827
Júlio César <juliocesarsouzacarvalho284@gmail.com>
2025-04-10 14:51:47 -0300

    Changing CSS and inserting context into html page

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from contact.models import Contact
 
 def index(request):
+    contact = Contact.objects.all()
+
+    context = {'contacts': contact,}
+
     return render(
         request,
         'contact/index.html',
+        context
     )
