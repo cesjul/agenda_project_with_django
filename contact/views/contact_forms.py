536c3870d3c6a575d7032049af8a93ee6918762d
Júlio César <juliocesarsouzacarvalho284@gmail.com>
2025-04-23 15:37:23 -0300

    Adding create form for contacts

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -3,11 +3,33 @@ from django.db.models import Q
 from contact.models import Contact
 from django.http import Http404
 from django.core.paginator import Paginator
+from django import forms
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = 'first_name', 'last_name', 'phone', 
 
 def create(request):
-    
+
+    if request.method == 'POST':
+        context = {'site_title': 'Create Contact | ',
+               'form': ContactForm(request.POST),
+               }
+        
+        return render(
+        request,
+        'contact/create.html',
+        context
+    )
+
+    context = {'site_title': 'Create Contact | ',
+               'form': ContactForm(),
+               }
+
+
     return render(
         request,
         'contact/create.html',
-        
+        context
     )
