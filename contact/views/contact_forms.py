44079c916b5a10cb758cbbc2a1ba5385e0b5b981
Júlio César <juliocesarsouzacarvalho284@gmail.com>
2025-04-28 14:46:41 -0300

    Creating field erros (not validated yet)

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,14 +1,27 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.db.models import Q
+from typing import Any
+from django.shortcuts import render
 from contact.models import Contact
-from django.http import Http404
-from django.core.paginator import Paginator
+from django.core.exceptions import ValidationError
+
 from django import forms
 
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = 'first_name', 'last_name', 'phone', 
+
+    def clean(self) -> dict[str, Any]:
+        cleaned_data = self.cleaned_data
+
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Invalid name',
+                code='invalid'
+            )
+        )
+
+        return super().clean()
 
 def create(request):
 
