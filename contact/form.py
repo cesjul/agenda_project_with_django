7bcf16e7cd426e59864191c5603bac00372abae2
Júlio César <juliocesarsouzacarvalho284@gmail.com>
2025-04-28 15:13:54 -0300

    Updating form widget

--- contact/form.py
+++ contact/form.py
@@ -4,17 +4,23 @@ from django.core.exceptions import ValidationError
 from django import forms
 
 class ContactForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['first_name'].widget.attrs.update({
+            'placeholder': 'Your first name',
+        })
     class Meta:
         model = Contact
         fields = 'first_name', 'last_name', 'phone', 
 
-    def clean(self) -> dict[str, Any]:
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Invalid name',
-                code='invalid'
-            )
-        )
+    # def clean(self) -> dict[str, Any]:
+    #     self.add_error(
+    #         'first_name',
+    #         ValidationError(
+    #             'Invalid name',
+    #             code='invalid'
+    #         )
+    #     )
 
-        return super().clean()
+    #     return super().clean()
