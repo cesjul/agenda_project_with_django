b03d702e1e84cfebd457b8ecaa3f4b1f009e7766
Júlio César <juliocesarsouzacarvalho284@gmail.com>
2025-04-28 14:52:21 -0300

    Moving ContactForm to a single module

---
+++ contact/form.py
@@ -0,0 +1,20 @@
+from typing import Any
+from contact.models import Contact
+from django.core.exceptions import ValidationError
+from django import forms
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = 'first_name', 'last_name', 'phone', 
+
+    def clean(self) -> dict[str, Any]:
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Invalid name',
+                code='invalid'
+            )
+        )
+
+        return super().clean()
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,27 +1,6 @@
 from typing import Any
 from django.shortcuts import render
-from contact.models import Contact
-from django.core.exceptions import ValidationError
-
-from django import forms
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = 'first_name', 'last_name', 'phone', 
-
-    def clean(self) -> dict[str, Any]:
-        cleaned_data = self.cleaned_data
-
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Invalid name',
-                code='invalid'
-            )
-        )
-
-        return super().clean()
+from contact.form import ContactForm
 
 def create(request):
 
