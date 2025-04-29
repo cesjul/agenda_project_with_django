05a9d6f617d75d2f846f8911d39a5f50f71892fd
Júlio César <juliocesarsouzacarvalho284@gmail.com>
2025-04-29 13:47:45 -0300

    Validating first name field

--- contact/form.py
+++ contact/form.py
@@ -14,13 +14,31 @@ class ContactForm(forms.ModelForm):
         model = Contact
         fields = 'first_name', 'last_name', 'phone', 
 
-    # def clean(self) -> dict[str, Any]:
-    #     self.add_error(
-    #         'first_name',
-    #         ValidationError(
-    #             'Invalid name',
-    #             code='invalid'
-    #         )
-    #     )
+    def clean(self) -> dict[str, Any]:
+        first_name = self.cleaned_data.get('first_name')
+        last_name = self.cleaned_data.get('last_name')
+        
+        if first_name.lower() == last_name.lower(): #type: ignore
+            print(first_name)
+            print(last_name)
+            self.add_error(
+                'last_name',
+                ValidationError(
+                'Last name cannot be equals to first name',
+                code='invalid')
+            )
+        return super().clean()
+    
+    def clean_first_name(self):
+        first_name_received = self.cleaned_data.get('first_name')
 
-    #     return super().clean()
+        if first_name_received == 'ABC':
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'Invalid name',
+                    code='invalid'
+            )
+            )
+        
+        return first_name_received
